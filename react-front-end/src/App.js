665f6175320312aa8d408bf5c1c5cc7f6137177e
Nga Nguyen <ngasdragon@gmail.com>
2020-05-29 15:43:38 -0700

    Init Apollo Client

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import BookList from './components/BookList';
+
+
+import ApolloClient from 'apollo-boost';
+import {gql} from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8080/graphql',
+});
+
+// client.query({
+//     query: gql `
+//     {
+//       books{
+//         name
+//       }
+//     }
+//   `
+// })
+// .then(result => console.log(result));
+
 function App() {
   return (
-    <div className="App">
-      <h1>GraphQL 101</h1>
-      <BookList/>
-    </div>
+    <ApolloProvider client = {client}>
+      <div className="App">
+        <h1>GraphQL 101</h1>
+        <BookList/>
+      </div>
+    </ApolloProvider>
   );
 }
 
