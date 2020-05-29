eb06bcb09532f9515266b6edc24f6abbb0bdf485
Nga Nguyen <ngasdragon@gmail.com>
2020-05-29 01:46:12 -0700

    Setting up React Front-End

---
+++ react-front-end/src/components/BookList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function BookList() {
+  return (
+    <div>
+      <ul id="booklist">
+        <li>Book name</li>
+      </ul>
+    </div>
+  );
+}
+
+export default BookList;
